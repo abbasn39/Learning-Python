da27d39f10ff31c97bd842f117147cdfe32ad411
abbasn39 <abbasn39@gmail.com>
2025-06-02 21:46:10 +0500

    file added: File IO write Functions, FileIO.txt

---
+++ Comments- Rearrange every week.py
@@ -0,0 +1,6 @@
+# -=, +=, *=, /=  are all compound assignment operators
+
+# The operators 'or' and 'and' return one of their operands, not necessarily of the boolean type
+# Nonetheless, 'not' always returns a boolean value.
+
+
