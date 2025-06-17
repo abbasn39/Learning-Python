5d3a03d77557f3d99efecf8c48d4397622e37a1c
abbasn39 <abbasn39@gmail.com>
2025-06-17 12:14:29 +0500

    file modified: Datatypes

--- 2-Data types.py
+++ 2-Data types.py
@@ -1,17 +1,17 @@
 Datatype1=int("5")
-print(type(Datatype1),"\t\t",Datatype1)                       #Datatype integer
+print(type(Datatype1),"\t\t",Datatype1)                     #Datatype integer
 
 Datatype2=float("3.14")
-print(type(Datatype2),"\t",Datatype2)                 #Datatype float
+print(type(Datatype2),"\t",Datatype2)                       #Datatype float
 
 
 Datatype3=str(42)
-print(type(Datatype3),"\t\t",Datatype3)                   #Datatype string
+print(type(Datatype3),"\t\t",Datatype3)                     #Datatype string
 
 
 Datatype4=bool(0)
-print(type(Datatype4),"\t\t",Datatype4)                    # Boolean '1' is true '0' is false
+print(type(Datatype4),"\t\t",Datatype4)                     # Boolean '1' is true '0' is false
 
 Datatype5=list("abc")
-print(type(Datatype5),"\t\t",Datatype5)                    #Datatype List, Stores multiple items in a Variable.
+print(type(Datatype5),"\t\t",Datatype5)                     #Datatype List, Stores multiple items in a Variable.
 
