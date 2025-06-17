244d3c1f5bca72b827cf7dc402c6abb4e6e15bdc
abbasn39 <abbasn39@gmail.com>
2025-06-17 13:06:35 +0500

    file modified: Datatypes

--- 2-Data types.py
+++ 2-Data types.py
@@ -15,4 +15,3 @@ print(type(Datatype4),"\t\t",Datatype4)                     # Boolean '1' is tru
 Datatype5=list("abc")
 print(type(Datatype5),"\t\t",Datatype5)                     #Datatype List, Stores multiple items in a Variable.
 
-print("Abbas")
