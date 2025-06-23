5e26e4e70998dcea7b8c178902dd32969cd18e0e
abbasn39 <abbasn39@gmail.com>
2025-06-23 12:08:42 +0500

    file modified: Iteration and recursion

--- 22- Iteration and Recursion.py
+++ 22- Iteration and Recursion.py
@@ -11,9 +11,16 @@ print(iterative_factorial(5))
 print("recursive approach")
 
 def recursive_factorial(x):
-    if x==1:
+    if x==1 or x==0:
         return 1
     else:
         return x*recursive_factorial(x-1)
 
-print(recursive_factorial())
+print(recursive_factorial(5))
+
+#Recursive function steps for the above recursive factorial program
+# (5*(recursive_factorial(5-1)))                    Step 1
+# (5*(4*recursive_factorial(4-1)))                  Step 2
+# (5*(4*(3*recursive_factorial(3-1))))              Step 3
+# (5*(4*(3*(2*recursive_factorial(2-1))))           Step 4
+# (5*(4*(3*(2*(1))))))                              Step 5
