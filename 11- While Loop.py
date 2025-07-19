9e561400e39f77d25442d1a0daec2c17a1268e48
abbasn39 <abbasn39@gmail.com>
2025-07-19 17:50:04 +0500

    File modified: while loops

--- 11- While Loop.py
+++ 11- While Loop.py
@@ -1,10 +1,15 @@
 i = 1
 
-while i <= 5:               #While loop will keep running until the value is less than or equal to 5.
+while i <= 5:               #While loop will keep running until condition given is satisfied.
     print(i)
-    i=i+1                   # This keeps adding 1 to 'i' and storing it in 'i' each time the while loop runs.
+    i = i + 1               # This keeps adding 1 to 'i' and storing it in 'i' each time the while loop runs.
 
 
-
+while True:                 # Condition for infinite loop 'True'
+    num=int(input("Enter your number: \n"))
+    if num==25:
+        break               # Can use 'break' to exit the loop early
+    else:
+        continue            # 'continue' keeps the loop running.
 
 
