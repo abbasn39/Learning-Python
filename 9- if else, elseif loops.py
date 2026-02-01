b0b4419be0261edc4ffc973f653b7b92eb38c96f
abbasn39 <abbasn39@gmail.com>
2026-02-01 15:36:35 +0500

    File modified: exp

--- 9- if else, elseif loops.py
+++ 9- if else, elseif loops.py
@@ -1,42 +1,43 @@
-print("Remove comment from one of the programs to execute it")
+print("\nRemove comment from one of the programs to execute it")
 
+#print("\nProgram 1\n")
+# print("This Program will check if the number you enter is less than or greater than 55")
+# X=2
+# Y=55
+#
+# Z=int(input("Please enter the number you want to check\n\n"))
+#
+# if Z>Y:
+#     print("The number is greater than 55")
+# elif Z==Y:
+#     print("The number is equal to 55")
+# else:
+#     print("The number is less than 55")
+#
 
-"""print("This Program will check if the number you enter is less than or greater than 55")
-X=2
-Y=55
-
-Z=int(input("PLease enter the number you want to check\n\n"))
-
-if Z>Y:
-    print("The number is greater than 55")
-elif Z==Y:
-    print("The number is equal to 55")
-else:
-    print("The number is less than 55")
-    """
-
-"""
-List_A= [1,2,3,4,5,6,7,8,9,10]
-
-if int(input("Please enter the number you want to check\n\n")) in List_A:
-    print("Yes this Number is in the list")
-
-else:
-    print("No this Number is not in the list")
-"""
-
-"""
-Age=float(input("Please enter your age\n\n"))
-if 7<=Age<=100:
-    if Age>18:
-        print("Yes, you are allowed to drive if you have a license")
-    elif Age==18:
-        print("Please contact us as we have to physically verify your age")
-    else:
-        print("No,you can not drive because you are under 18")
-else:
-    print("Please only enter a number between 7 and 100")
-"""
+#print("\nProgram 2\n")
+# List_A= [1,2,3,4,5,6,7,8,9,10]
+#
+# if int(input("Please enter the number you want to check\n\n")) in List_A:
+#     print("Yes this Number is in the list")
+#
+# else:
+#     print("No this Number is not in the list")
+#
+#
+##print("\nProgram 3\n")
+#
+# Age=float(input("Please enter your age\n\n"))
+# if 7<=Age<=100:
+#     if Age>18:
+#         print("Yes, you are allowed to drive if you have a license")
+#     elif Age==18:
+#         print("Please contact us as we have to physically verify your age")
+#     else:
+#         print("No,you can not drive because you are under 18")
+# else:
+#     print("Please only enter a number between 7 and 100")
+#
 
                     # if condition1:
                     #     # code runs if condition1 is True
@@ -53,7 +54,7 @@ else:
                     # >=    (Greater than or equal to)
                     # <=    (Less than or Equal to)
 
-                    # You can also use 'and' between the above conditions like 18>=Age and Age<=30   Range from 18-30
+                    # You can also use 'and' between the above conditions like 18>=Age and Age<=30, Range from 18-30
 
 
 # SHORTHAND IF ELSE STATEMENTS
