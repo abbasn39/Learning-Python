9b3e3c3993c9b7ab09b32783646b814ea038f264
abbasn39 <abbasn39@gmail.com>
2026-02-14 13:53:37 +0500

    File created: Tkinter counting button

--- Modules/Tkinter/Counting_button.py
+++ Modules/Tkinter/Counting_button.py
@@ -10,14 +10,16 @@ def counter():
 
 window_button = Tk()                # Initializing a Window
 
-count_button=Button(window_button,text='Press me to count') # Initializing a button
-count_button.config(command=counter)
-count_button.config(font=("Arial",20,"italic"))                         # Integrating a command which is to callback function
-                                                            # called 'counter'
+count_button=Button(window_button,text='Press me to count')     # Initializing a button
+count_button.config(command=counter)                            # Integrating a command which is to callback function
+                                                                # called 'counter'
+count_button.config(font=("Arial",20,"italic"))
 
-label=Label(window_button,text=count,
-            font=('Monospace',50,"bold"))          # Initializing a Label and setting its text to
-                                                            # variable 'count' (line 2)
+
+label=Label(window_button,text=count,                           # Initializing a Label and setting its text to
+                                                                # variable 'count' (line 2)
+            font=('Monospace',50,"bold"))
+
 
 label.pack()
 count_button.pack()
