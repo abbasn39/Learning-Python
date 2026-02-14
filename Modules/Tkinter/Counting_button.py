2d50866b73ee2ff4f015be20fe196a660d13cbd5
abbasn39 <abbasn39@gmail.com>
2026-02-14 23:10:48 +0500

    File modified: counting button

--- Modules/Tkinter/Counting_button.py
+++ Modules/Tkinter/Counting_button.py
@@ -7,22 +7,39 @@ def counter():
     print(count)                    # This will print the 'count' variable on console
     label.config(text=count)        # This will print on the label inside the window
 
+def reset():
+    global count
+    count=0
+    print(count)
+    label.config(text=count)
+
+def count_back():
+    global count
+    count -=1
+    print(count)
+    label.config(text=count)
 
 window_button = Tk()                # Initializing a Window
+window_button.title("Counter")
 
 count_button=Button(window_button,text='Press me to count')     # Initializing a button
 count_button.config(command=counter)                            # Integrating a command which is to callback function
                                                                 # called 'counter'
 count_button.config(font=("Arial",20,"italic"))
 
+count_button.pack()
 
 label=Label(window_button,text=count,                           # Initializing a Label and setting its text to
                                                                 # variable 'count' (line 2)
             font=('Monospace',50,"bold"))
-
-
 label.pack()
-count_button.pack()
 
+reset_button=Button(window_button, text="reset counter",font=('Arial',10,"bold"))
+reset_button.config(command=reset)
+reset_button.pack(side="right")
+
+count_back_button = Button(window_button, text="count back",font=('Arial',10,"bold"))
+count_back_button.config(command=count_back)
+count_back_button.pack(side="left")
 
 window_button.mainloop()
