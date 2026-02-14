e9a6832c3c248bd1e9b96d382e15b9d5d37b2c37
abbasn39 <abbasn39@gmail.com>
2026-02-14 13:52:00 +0500

    File created: Tkinter counting button

--- Modules/Tkinter/Counting_button.py
+++ Modules/Tkinter/Counting_button.py
@@ -1,17 +1,26 @@
 from tkinter import *
 
-def count():
-    pass
+count=0                             # Initialize a variable
+def counter():
+    global count                    # make the variable global so we can use inside function
+    count += 1
+    print(count)                    # This will print the 'count' variable on console
+    label.config(text=count)        # This will print on the label inside the window
 
 
-window_button = Tk()
+window_button = Tk()                # Initializing a Window
 
-count_button=Button(window_button,text='Press me to count')
+count_button=Button(window_button,text='Press me to count') # Initializing a button
+count_button.config(command=counter)
+count_button.config(font=("Arial",20,"italic"))                         # Integrating a command which is to callback function
+                                                            # called 'counter'
 
+label=Label(window_button,text=count,
+            font=('Monospace',50,"bold"))          # Initializing a Label and setting its text to
+                                                            # variable 'count' (line 2)
 
-
-
-
+label.pack()
+count_button.pack()
 
 
 window_button.mainloop()
